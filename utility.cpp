c28acf3d6ef41e66ce8d299b26d4ed6b998fde49
Saptarshi <saptarshi.nag01@gmail.com>
2023-03-10 15:12:29 +0530

    Empty Commit for Backup

---
+++ utility.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <random>
+#include <utility>
+
+// for std::move()
+int main()
+{
+
+
+Customer c{"Wolfgang Amadeus Mozart" };
+for (int val : {0, 8, 15}) {
+c.addValue(val);
+}
+std::cout << "c: " << c << ' \n ' ;
+// print value of initialized c
+// insert the customer twice into a collection of customers:
+std::vector<Customer> customers;
+customers.push_back(c);
+// copy into the vector
+customers.push_back(std::move(c)); // move into the vector
+std::cout << "c: " << c << ' \n ' ;
+// print value of moved-from c
+// print all customers in the collection:
+std::cout << "customers:\n";
+for (const Customer& cust : customers) {
+std::cout << " " << cust << ' \n ' ;
+}
+}
